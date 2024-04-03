79d2784538eab44cb7e00375772ce7edad5a7f3a
ugimser <164077841+ugimser@users.noreply.github.com>
2024-04-03 13:36:23 +0200

    Update ActionTest.js

--- js/regex/ActionTest.js
+++ js/regex/ActionTest.js
@@ -1,1 +1,14 @@
 // Change it
+const fs = require('fs');
+
+// Aktualna data i czas
+const currentDate = new Date().toString();
+const content = `Ostatnia aktualizacja: ${currentDate}`;
+
+// Ścieżka do pliku
+const filePath = 'ActionTest.js';
+
+// Zapisanie danych do pliku
+fs.writeFileSync(filePath, content);
+
+console.log('Plik został zaktualizowany.');
