da27b0d83a0f1e40d0c00cbca26300ca6aebfdb3
ugimser <164077841+ugimser@users.noreply.github.com>
2024-04-05 21:09:32 +0200

    Update processFile.js

--- processFile.js
+++ processFile.js
@@ -1,10 +1,13 @@
 console.log('processFile.js 123');
 
 const { uniqueItemArray } = require('./js/regex/UniqueItems.js');
+const { coffinArray } = require('./js/regex/Coffin.js');
 
 const uniqueArray = uniqueItemArray;
+const coffinArrayTemp = coffinArray;
 //console.log(uniqueItemArray.length);
 changeArray ();
+
 
 function changeArray () {
     const fs = require('fs');
@@ -48,6 +51,7 @@ function changeArray () {
          console.log('changePrices (lTab, 0)', err);
     }
 
+    // save Unique list for Gewnnen
     let string = '';
     for (const item of uniqueArray) {
        string += `{ id: ${item.id}, baseType: "${item.baseType}", shortName: "${item.shortName}",  name: "${item.name}", image: "${item.image}", chaosValueLeague: ${item.chaosValueLeague}, chaosValueHCLeague: ${item.chaosValueHCLeague}, chaosValueStandard: ${item.chaosValueStandard} },\n`;
@@ -57,6 +61,33 @@ function changeArray () {
  
     try {
         fs.writeFileSync('Ninja_json/GeneratedUniqueArray.js', strAll, 'utf8');
+    } catch (err) {
+        console.log('fs.writeFileSync ', err);
+    }
+
+    // Coffin
+    // League
+    // League HC
+    const coffinA = fs.readFileSync('Ninja_json/Necropolis_Coffin.json', 'utf8');
+    const coffinB = fs.readFileSync('Ninja_json/Hardcore_Necropolis_Coffin.json', 'utf8');
+    const coffinTab = [coffinA, coffinB];
+    console.log('coffinTab done');
+    try {
+        changePrices (coffinTab, 0);
+    } catch (err) {
+         console.log('changePrices (lTab, 0)', err);
+    }
+
+    // save Coffin list
+    string = '';
+    for (const item of coffinArrayTemp) {
+       string += `{ id: ${item.id}, name: "${item.name}", regex: "${item.regex}", image: "${item.image}", ilvl: "${item.ilvl}" chaosValueLeague: ${item.chaosValueLeague}, chaosValueHCLeague: ${item.chaosValueHCLeague}, chaosValueStandard: ${item.chaosValueStandard} },\n`;
+    }
+    
+    const strAll = `const coffinArray2 = [\n ${string}];\n\n if (typeof module !== 'undefined' && module.exports) { module.exports = { coffinArray2 }; }`;
+ 
+    try {
+        fs.writeFileSync('Ninja_json/GeneratedCoffinArray.js', strAll, 'utf8');
     } catch (err) {
         console.log('fs.writeFileSync ', err);
     }
